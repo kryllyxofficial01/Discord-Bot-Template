bc154c57e2ae9d81a5434105d9bfc23782637703
Andrew Campbell <97801783+kryllyxofficial01@users.noreply.github.com>
2022-04-17 17:41:33 -0400

    template

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+# Connects your code to the bot. Treat it like a password.
+TOKEN = "enter token here (see README on how to get it)"
+
+client = commands.Bot(command_prefix="!") # You can change the prefix if you want.
+
+# When the code successfully connects, this message will get printed.
+@client.event
+async def on_ready():
+  print("Logged on as {0.user}.".format(client))
+
+# The bot will reply with "pong" when a user types "!ping".
+@client.command()
+async def ping(ctx):
+  await ctx.send("pong")
+
+client.run(TOKEN)
